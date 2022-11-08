0a58030a814a35d1d097ae580198c0af15a94f38
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-08 12:39:02 -0300

    add ul, li styles

--- src/view/Navbar.tsx
+++ src/view/Navbar.tsx
@@ -3,8 +3,15 @@
 
 const Navbar = () => {
   return (
-    <div className='w-full text-white h-20 justify-between items-center px-8'>
+    <div className='w-full text-white h-20 justify-between items-center px-8 flex'>
       <h1 className="text-5xl font-bold text-[#00df9a]">LOGO-</h1>
+      <ul className="flex items-center">
+        <li className="p-4">Services</li>
+        <li className="p-4">Banking</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Recources</li>
+        <li className="p-4"></li>
+      </ul>
     </div>
   )
 }
