eba6e475f4d01586f98d1f6ad50f4cf449858c0c
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-08 17:52:57 -0300

    a

--- src/view/Navbar.tsx
+++ src/view/Navbar.tsx
@@ -11,7 +11,51 @@ const Navbar = () => {
     <div className='w-full text-white h-20 justify-between items-center px-8 flex'>
       <h1 className="text-5xl font-bold text-[#00df9a]">LOGO-</h1>
       <ul className="flex items-center">
-        <li className="p-4">Services</li>
+        <li className="p-4">
+          <Menu as="div">
+            <Menu.Button>
+              Services
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute  z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      For Companies
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      For Invertors
+                    </a>
+                  )}
+                </Menu.Item>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </li>
         <li className="p-4">Banking</li>
         <li className="p-4">Company</li>
         <li className="p-4">Recources</li>
@@ -21,7 +65,7 @@ const Navbar = () => {
             <div>
               <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
                 Options
-                <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" /> 
+                <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
               </Menu.Button>
             </div>
 
