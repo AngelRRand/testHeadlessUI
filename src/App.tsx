15c6a4056cd0e2b985a8063e758e29bb5a0f47bf
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-08 12:37:20 -0300

    add styles navbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <h1 className='text-3xl font-bold underline'>asda</h1>
     </div>
   )
 }
--- src/view/Navbar.tsx
+++ src/view/Navbar.tsx
@@ -3,8 +3,8 @@
 
 const Navbar = () => {
   return (
-    <div className='w-full text-white'>
-      <h1>HAS</h1>
+    <div className='w-full text-white h-20 justify-between items-center px-8'>
+      <h1 className="text-5xl font-bold text-[#00df9a]">LOGO-</h1>
     </div>
   )
 }
